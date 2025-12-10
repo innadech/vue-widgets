2819c65e7148f45654880e2ad534f50be6a41baa
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-12-10 02:24:35 +0200

    Added: root template

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
-<script setup></script>
+<script>
+export default {
+  data() {
+    return {
+      flowers: [],
+      fruits: [],
+      drinks: [],
+    }
+  },
+}
+/**
+ * реализовать логику:
+ *  добавление элемента
+ *  удаление элемента
+ *  редактирование элемента
+ *  просмор списка
+ *
+ * нарезаем на компоненты
+ */
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>APP WIDGETS</h1>
+  <div>
+    <h2>Flowers</h2>
+    <ul>
+      <li>Rose</li>
+    </ul>
+  </div>
+  <div>
+    <h2>Fruits</h2>
+    <ol>
+      <li>Orange</li>
+    </ol>
+  </div>
+  <div>
+    <h2>Drinks</h2>
+    <section>
+      <article>Water</article>
+    </section>
+  </div>
 </template>
-
-<style scoped></style>
