f846ffd89097439c2c0dd72d52ea86e9f01c23d4
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-12-17 02:37:29 +0200

    Implemented UiSelect

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,22 @@
 import FruitWidget from './components/fruit-widget/FruitWidget.vue'
 import DrinkWidget from './components/drink-widget/DrinkWidget.vue'
 import FlowerWidget from './components/flower-widget/FlowerWidget.vue'
+import UiSelect from './UiSelect.vue'
 
 export default {
   components: {
     FruitWidget,
     FlowerWidget,
     DrinkWidget,
+    UiSelect,
   },
 }
 </script>
 
 <template>
   <h1>APP WIDGETS</h1>
-
-  <FruitWidget />
+  <UiSelect v-on:country-updated="console.log($event)"></UiSelect>
+  <!-- <FruitWidget />
   <FlowerWidget />
-  <DrinkWidget />
+  <DrinkWidget /> -->
 </template>
---
+++ src/UiSelect.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  emits: ['country-updated'],
+
+  data() {
+    return {
+      availableCountries: ['France', 'Germany', 'Spain', 'Australia', 'China'],
+      selectedCountry: 'France',
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ selectedCountry }}</h1>
+    <select
+      v-bind:value="selectedCountry"
+      v-on:change="selectedCountry = $event.target.value"
+    >
+      <option v-for="ac of availableCountries" :key="ac">{{ ac }}</option>
+    </select>
+    <button v-on:click="selectedCountry = 'Australia'">
+      PUSH ME to Australia
+    </button>
+    <button v-on:click="$emit('country-updated', selectedCountry)">
+      CONFIRM!
+    </button>
+  </div>
+</template>
